9d997872a9eeb822e332c6da7a442123bce73c7c
有種 <39034242+kenzok8@users.noreply.github.com>
2026-04-04 21:07:33 +0800

    Fix repository name in check_luci_version.sh

--- clashoo/root/usr/share/clash/check_luci_version.sh
+++ clashoo/root/usr/share/clash/check_luci_version.sh
@@ -2,5 +2,5 @@
 
 . /usr/share/clash/version_lib.sh
 
-write_release_tag "kenzok78/luci-app-clashoo" "/usr/share/clash/new_luci_version"
+write_release_tag "kenzok8/luci-app-clashoo" "/usr/share/clash/new_luci_version"
  
